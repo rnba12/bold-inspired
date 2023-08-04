ce59a7279924cdc43a15aed268dc1920214b51ca
rnba12 <rnba12@yahoo.co.uk>
2023-08-05 01:14:51 +0200

    Posts

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,14 +1,16 @@
 import "./style.scss"
 
-export default function Post({title, author, image, featured}) {
+export default function Post({title, author, topic, image, body}) {
 
     return (
-        <div className={["post " + (featured ? "featured" : "")]}>
+        <div className={["post " + (body ? "featured" : "")]}>
             <img src={image} alt={title} width={200} height={"auto"}/>
             <div className="content">
+                <div className="topic">{topic}</div>
                 <div className="title">{title}</div>
                 <div className="author">{author}</div>
             </div>
+                <div>{body}</div>
         </div>
     )
 }
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -2,21 +2,46 @@ import Post from '../Post/Post'
 import "./style.scss"
 
 export default function Posts() {
-    
+
     return (
-        <>
+        <section style={{padding: "0 2.5rem"}}>
             <h2>Latest</h2>
             <div className="posts">
-                <div className="feature-post">
-                    <Post title={"Featured Post"} author={"Author Name"} image={"src/assets/stock1.webp"} featured={true}/>
+                <div className="feature-post" style={{ gridArea: "p2" }}>
+                    <Post title={"Featured Post"} author={"Author Name"} topic={"Culture"} image={"src/assets/slide3.jpg"} body={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus vero omnis optio nemo modi. Molestias perspiciatis, hic sed maiores nobis omnis illum provident, inventore impedit ipsa accusantium? Pariatur, temporibus officia!"} />
                 </div>
-                <div className="other-posts">
-                    <Post title={"Random Post Title 1"} author={"Author Name"} image={"src/assets/stock3.jpg"} featured={false}/>
-                    <Post title={"Post 2"} author={"Author Name"} image={"src/assets/stock2.jpg"} featured={false}/>
-                    <Post title={"Post 3"} author={"Author Name"} image={"src/assets/stock4.jpg"} featured={false}/>
-                    <a href="#" className='more-btn'>See More <iconify-icon icon="ph:arrow-right-bold"></iconify-icon></a>
+
+                <div style={{gridArea: "p1"}}>
+                    <Post title={"Random Post Title 1"} author={"Author Name"} topic={"Sport"} image={"src/assets/slide2.jpg"} body={false}
+                    />
+                </div>
+                <div style={{gridArea: "p3"}}>
+                    <Post
+                        title={"Post 2"}
+                        author={"Author Name"}
+                        topic={"Music"}
+                        image={"src/assets/stock2.jpg"}
+                        body={false}
+                    />
+                </div>
+                <div style={{gridArea: "p4"}}>
+                    <Post
+                        title={"Post 3"}
+                        author={"Author Name"}
+                        topic={"Culture"}
+                        image={"src/assets/stock4.jpg"}
+                        body={false} />
+                </div>
+                <div style={{gridArea: "p5"}}>
+                    <Post
+                        title={"Post 4"}
+                        author={"Author Name"}
+                        topic={"Culture"}
+                        image={"src/assets/stock1.webp"}
+                        body={false} />
                 </div>
             </div>
-        </>
+
+        </section>
     )
 }
