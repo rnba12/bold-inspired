3ce037724a704febeae11232ee82213fbaefa224
rnba12 <rnba12@yahoo.co.uk>
2023-08-14 12:06:56 +0100

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,31 @@ export default function Footer() {
 
     return (
         <footer>
-            <span>© Bold Inspired 2023</span>
+           <div className="footer-logo">
+            <span>BOLD INSPIRED</span> ©<br/>Inspiring, Informing, Illuminating
+           </div>
+           <div className="footer-content">
+            <div className="links">
+                <ul>
+                    <li>About</li>
+                    <li>Subscribe</li>
+                    <li>Blog</li>
+                    <li>FAQ</li>
+                </ul>
+                <ul>
+                    <li>Sitemap</li>
+                    <li>Privacy</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className="socials">
+                <span><iconify-icon icon="bxl:instagram"></iconify-icon></span>
+                <span><iconify-icon icon="bxl:twitter"></iconify-icon></span>
+                <span><iconify-icon icon="bxl:facebook"></iconify-icon></span>
+                <span><iconify-icon icon="bxl:tiktok"></iconify-icon></span>
+                <span><iconify-icon icon="bxl:youtube"></iconify-icon></span>
+            </div>
+           </div>
 
         </footer>
     )
